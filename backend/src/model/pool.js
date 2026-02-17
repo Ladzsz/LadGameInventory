eb48b6e8bd2ba52b68c5ebec80bae9b7546605b9
London Mason <londonmason602@gmail.com>
2026-02-17 16:47:02 -0500

    added documentation to pool
    Added comment to clarify the use of DATABASE_URL for production.

--- backend/src/model/pool.js
+++ backend/src/model/pool.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
- connectionString: process.env.DATABASE_URL
+ connectionString: process.env.DATABASE_URL //PRODUCTION VERSION for future reference use single url strings
 });
 
 module.exports = pool;
