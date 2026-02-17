f97ef2d2191f4bd6589e5641aff8ae03b853e26c
London Mason <londonmason602@gmail.com>
2026-02-17 16:42:12 -0500

    Refactor database connection to use connectionString for easier production

--- backend/src/model/pool.js
+++ backend/src/model/pool.js
@@ -1,11 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+ connectionString: process.env.DATABASE_URL
 });
 
 module.exports = pool;
